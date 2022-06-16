0508111ab3e037c94925040a99a45479f7d9790a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-06-16 23:26:35 +0900

    refactor to tsx

--- population_app/src/components/Main.tsx
+++ population_app/src/components/Main.tsx
@@ -3,20 +3,31 @@ import axios from 'axios';
 import { Checkfield } from './CheckField';
 import { Test } from './Test';
 
-export const Main = () => {
+interface Props {};
+
+interface Prefecture {
+  prefCode: number;
+  prefName: String;
+};
+
+export const Main: React.FC = () => {
   const API_KEY = process.env.REACT_APP_RESAS_API_KEY;
-  const [prefectures, setPrefectures] = useState([]); //県名初期化
+  const [prefectures, setPrefectures] = useState<Prefecture[]>([]); //県名初期化
   
   //都道府県の一覧の取得
   useEffect(()=>{
+    if (API_KEY) {
       axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures",{
-          headers:{'X-API-KEY': API_KEY},
+          headers:{ 'X-API-KEY': API_KEY },
       })
       .then((res)=>{
         setPrefectures(res.data.result);
       })
       .catch((error)=>{})
+    }
   }, [])
+
+  console.log(prefectures)
 
   return (
     <>
