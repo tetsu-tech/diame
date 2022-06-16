6e280a227569ff6f69a74dd820a96b13240a911a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-06-16 21:54:05 +0900

    一旦commit

--- population_app/src/components/Main.jsx
+++ population_app/src/components/Main.jsx
@@ -3,25 +3,25 @@ import axios from 'axios';
 import { Checkfield } from './CheckField';
 
 export const Main = () => {
-  require('dotenv').config()
-  console.log(process.env)
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  console.log(API_KEY)
-    const [prefectures,setPrefectures] = useState([]); //県名初期化
-    
-    //都道府県の一覧の取得
-    useEffect(()=>{
-        axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures",{
-            headers:{'X-API-KEY':API_KEY},
-        })
-        .then((results)=>{
-          setPrefectures(results.data);
-        })
-        .catch((error)=>{})
-    },[])
+  const API_KEY = process.env.REACT_APP_RESAS_API_KEY;
+  const [prefectures, setPrefectures] = useState([]); //県名初期化
+  
+  //都道府県の一覧の取得
+  useEffect(()=>{
+      axios.get("https://opendata.resas-portal.go.jp/api/v1/prefectures",{
+          headers:{'X-API-KEY':API_KEY},
+      })
+      .then((res)=>{
+        setPrefectures(res.data);
+      })
+      .catch((error)=>{})
+  }, [])
+
+
   return (
     <>
-      <Checkfield prefectures={prefectures.result} />
+      {/* <Checkfield prefectures={prefectures.result} /> */}
+      <div>hogehgoe</div>
     </>
   )
 }
